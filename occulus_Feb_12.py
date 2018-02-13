d835aa0578ab2b8ac200e6318b5976dfd096d3d4
hli2020 <hongyangli2020@gmail.com>
2018-02-13 09:36:36 +0800

    udpate

---
+++ occulus_Feb_12.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isMatch(self, text, pattern):
+        if not pattern:
+            return not text
+
+        first_match = bool(text) and pattern[0] in {text[0], '.'}
+
+        if len(pattern) >= 2 and pattern[1] == '*':
+            return (self.isMatch(text, pattern[2:]) or
+                    first_match and self.isMatch(text[1:], pattern))
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
+
+sln = Solution()
+text = 'aabbbbbbbbbbbbbbbbbb'
+pattern = 'e*a*b*'
+damn = sln.isMatch(text, pattern)
+
+a = 1
+import List
